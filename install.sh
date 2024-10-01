fcad892e1a1321f6b097821da7fa90745bd6bf01
uriid1 <appdurov@gmail.com>
2024-10-01 22:21:29 +0300

    Update installer

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
 printf "Generate Keys\n"
+mkdir keys
 ssh-keygen -t ecdsa -b 521 -f keys/ssh_host_ecdsa_key -N ""
 printf "\n"
 
@@ -10,7 +11,7 @@ printf "\n"
 
 printf "Copy to /home/$USER/honeypot-ssh-server\n"
 mkdir ~/honeypot-ssh-server
-cp -r log ~/honeypot-ssh-server
+mkdir ~/honeypot-ssh-server/log
 cp -r keys ~/honeypot-ssh-server
 cp honeypot-ssh-server ~/honeypot-ssh-server
 printf "\n"
